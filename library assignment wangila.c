b9e3a3dc14c200d7ff9f7c6f9f3c783493b876ed
wangila001 <ezekielwangila2@gmail.com>
2025-02-28 05:31:44 -0600

    Add files via upload

---
+++ library assignment wangila.c
@@ -0,0 +1,39 @@
+/* 
+NAME: ROWLAND WENDO 
+ADMISSION:D33-0263-2022 
+*/
+#include<stdio.h>
+int main() {
+int bookID,dueDate,returnDate,daysOverdue,fineRate,fineAmount;
+printf("enter the book id: \n");
+scanf("%d",&bookID);
+printf("enter due date:\n");
+scanf("%d",&dueDate);
+printf("enter the return date: \n");
+scanf("%d",& returnDate);
+//to calculate the days overdue 
+daysOverdue = returnDate - dueDate;
+if(daysOverdue<=0){
+fineRate=0;
+fineAmount=0;
+}
+else if ( daysOverdue>0 && daysOverdue<=7) {
+fineRate= 20;
+fineAmount= daysOverdue *fineRate;
+}
+else if ( daysOverdue >=8 && daysOverdue<=14){
+fineRate=50;
+fineAmount =daysOverdue * fineRate;
+}
+else {
+fineRate= 100;
+fineAmount = daysOverdue * fineRate;
+}
+printf("bookID is : %d \n",bookID);
+printf("the due date is : %d \n",dueDate);
+printf("return date is : %d \n", returnDate);
+printf("the days overdue are : %d \n",(daysOverdue>0) ? daysOverdue:0);
+printf("the fineRate is ksh %d per day \n", fineRate);
+printf("the fineAmount is ksh %d \n",fineAmount);
+return 0;
+}
